7c5aa0c503866be6df3ecd1ec7b4f253738dd500
sauron <kravshanov97@gmail.com>
2025-02-15 01:42:26 +0500

    added some changes

--- config/settings.py
+++ config/settings.py
@@ -72,11 +72,13 @@ TEMPLATES = [
 
 # WSGI_APPLICATION = 'config.wsgi.application'
 ASGI_APPLICATION = 'config.asgi.application'
+
 CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
             "hosts": [("127.0.0.1", 6379)],
+
         },
     },
 }
